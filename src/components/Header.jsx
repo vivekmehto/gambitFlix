3069101deb132a9469b9db25a5165017a9cd758a
vivekmehto <vivekmehto.webdev@gmail.com>
2025-11-24 15:34:12 +0530

    ADD: Header and login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-center bg-black'>
-
-      <img 
-      className='w-32'
+    <div className=' w-full absolute bg-linear-to-b from-black to-transparent p-4 space-x-2 z-10'>
+      <div className='flex items-center gap-0'>
+  {/* <img 
+      className='w-32 cursor-pointer m-0'
       src='logo-rmbg.png'
       alt='Gambit Flix Logo'
-      />
+      /> */}
 
-      <h1 className='text-2xl font-bold font- text-white '>Gambit Flix</h1>
+      <h1 className='text-4xl p-2 font-bold text-red-700 '>♞ GambitFlix</h1>
+    
+      </div>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="relative h-screen w-full">
+      <Header />
 
-export default Login
+      {/* Background Image */}
+      <img
+        className="h-full w-full object-cover brightness-50"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_large.jpg"
+        alt="Login Banner"
+      />
+
+      {/* Centered Form */}
+      <form
+        className="
+          absolute top-1/2 left-1/2 
+          -translate-x-1/2 -translate-y-1/2
+          w-[90%] max-w-md
+          bg-black/70 p-10 rounded-lg text-white
+          shadow-xl
+        "
+      >
+        <h2 className="text-3xl font-bold mb-6">Sign In</h2>
+
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="p-3 mb-4 w-full rounded bg-gray-800 text-white outline-none"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 mb-6 w-full rounded bg-gray-800 text-white outline-none"
+        />
+
+        <button className="bg-red-600 hover:bg-red-700 w-full p-3 rounded font-semibold">
+          Login
+        </button>
+
+        <p className="mt-4 text-gray-300 text-sm">
+          New to GambitFlix?{" "}
+          <span className="text-white font-medium cursor-pointer">Sign up now</span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
