8112cdd3ef4cfc32331f6c53bd7dc3259df2aa0f
vivekmehto <vivekmehto.webdev@gmail.com>
2025-12-02 12:40:38 +0530

    ADD: movie store and tmdb api

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch(); 
+
+  const getNowPlayingMovies = async () => {
+    try {
+      const data = await fetch( 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS );
+      const jsonData = await data.json();
+      console.log(jsonData);
+      dispatch(addNowPlayingMovies(jsonData.results));
+
+    } catch (error) {
+      console.error("Error fetching now playing movies:", error);
+    } 
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+}
+
+export default useNowPlayingMovies;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTBiMGQ0YWNjNzg4MjkyMDk4NTMxMjUwM2RhYzc5MiIsIm5iZiI6MTc2NDY1MjI5OC43OTksInN1YiI6IjY5MmU3NTBhYmE0Nzk5YjA2ODZlMmNkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KNAGDsPWRl2Hwx-nIan-XW7dJ13iK0H9kc5pdCrqdw'
+  }
+};
