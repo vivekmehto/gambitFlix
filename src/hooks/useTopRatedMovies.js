2b446b903cc51d2dc7a225adeb44b87fcbc6eded
vivekmehto <vivekmehto.webdev@gmail.com>
2025-12-03 11:57:50 +0530

    ADD: upcoming hook

---
+++ src/hooks/useTopRatedMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTopRatedMovies } from "../utils/movieSlice";
+import { useSelector } from "react-redux";
+
+
+const useTopRatedMovies = () => {
+    const dispatch = useDispatch();
+
+    const topRatedMovies = useSelector((store) => store.movies.topRatedMovies);
+
+    const getTopRatedMovies = async () => {
+        const data = await fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+             API_OPTIONS);
+        const json = await data.json();
+        console.log("Top Rated Movies Data:", json);
+        dispatch(addTopRatedMovies(json.results));
+    }
+
+    useEffect(() => {
+        !topRatedMovies && getTopRatedMovies();
+    }, []);
+}
+
+export default useTopRatedMovies
---
+++ src/hooks/useUpcomingMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addUpcomingMovies } from "../utils/movieSlice";
+import { useSelector } from "react-redux";
+
+
+const useUpcomingMovies = () => {
+    const dispatch = useDispatch();
+
+    const upcomingMovies = useSelector((store) => store.movies.upcomingMovies);
+
+    const getUpcomingMovies = async () => {
+        const data = await fetch("https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+             API_OPTIONS);
+        const json = await data.json();
+        console.log("Upcoming Movies Data:", json);
+        dispatch(addUpcomingMovies(json.results));
+    }
+
+    useEffect(() => {
+        !upcomingMovies && getUpcomingMovies();
+    }, []);
+}
+
+export default useUpcomingMovies
